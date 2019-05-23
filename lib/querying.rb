474758a3535ab7824a37134be7ad87f7534ea0a9
Joe Jamsky <cba1067950@yahoo.com>
2019-05-23 20:09:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT name FROM subgenres;"
+  "SELECT authors.name, subgenres.name
+  FROM subgenres
+  JOIN series ON ;"
 end
 
 def select_series_title_with_most_human_characters
