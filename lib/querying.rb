9c5fbaa85f7b815ccf626de44cac36a3ad5a6ca4
Joe Jamsky <cba1067950@yahoo.com>
2019-05-23 20:32:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,7 @@ def select_series_title_with_most_human_characters
   JOIN characters ON series_id = series.id
   WHERE characters.species = \"human\" 
   GROUP BY series.title 
-  WHERE COUNT(series.title) > 
+  HAVING COUNT(series.title) > 
    (SELECT max(COUNT(characters.species)) FROM characters)
   ;"
 end
